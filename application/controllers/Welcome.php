4a61dfa4c444b6d02944b90bda83056e4676ab38
Thanawat <thanawat_ben@hotmail.com>
2017-03-16 18:05:06 +0700

    -

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,23 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+use Facebook\InstantArticles\Client\Client;
+use Facebook\InstantArticles\Transformer\Transformer;
 class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+{
+    parent::__construct();
+		$ia_client = Client::create(
+	    '1920957664803342'
+	);
+	// Push the article to your Facebook Page
+	$ia_client->importArticle($my_article);
+	// Unpublish an article from your Facebook Page
+	$ia_client->removeArticle($canonical_url);
+}
 	public function index()
 	{
 		$this->load->view('index');
