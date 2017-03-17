bca1176e8f914bfb5150d47b22006a3f68b7b2bd
Thanawat <thanawat_ben@hotmail.com>
2017-03-17 11:30:32 +0700

    -

--- 1.php
+++ 1.php
@@ -27,20 +27,15 @@ $the_html = '<!doctype html>
 </body>
 </html>';
 
- $fb = Client::create(
-    '1335396193222019',
-    '45cb6b14299eafb282bdf42f4315ea33',
-    'EAACEdEose0cBAPXWhCFHglBEKeg7CRURbHgkNsIojSOLW5FZAdHnZBbTGTZAr6AO0EjgsimSL4zRGQqoUqfFlo7gvpweFZBQIStt2ZCJiTAKwHMZCqjec47fq62scGLQf6WR7rcaS1NsleEjWOC6sx3lOoS9hAGnC7Whnh2wnnArsl5JbkyqV8Yq9GP4QTrBYZD',
-    '1920957664803342',
-     true //development env
- );
+
 
 
 
 $rules = file_get_contents( "simple-rules.json", true);
-$instant_article = InstantArticle::create();
 $transformer = new Transformer();
 $transformer->loadRules($rules);
+$instant_article = InstantArticle::create();
+
 libxml_use_internal_errors(true);
 $document = new \DOMDocument();
 $document->loadHTML($the_html);
@@ -48,5 +43,5 @@ libxml_use_internal_errors(false);
 
 $transformer->transform($instant_article, $document);
 
-$fb->importArticle($instant_article, true);
+
 ?>
